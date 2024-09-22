4a918e529e6bdeaa15f23a77a7c33a6a05001d09
unyou125 <144504949+unyou125@users.noreply.github.com>
2024-09-22 01:41:28 -0700

    Delete api/files.js

--- api/files.js
+++
@@ -1,6 +0,0 @@
-// api/files.js
-export default async function handler(req, res) {
-    // Fetch the list of files from your storage solution
-    const files = []; // Replace this with your file fetching logic
-    res.status(200).json(files);
-}
